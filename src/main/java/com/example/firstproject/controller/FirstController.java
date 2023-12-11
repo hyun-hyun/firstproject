d5f8b75003e267ba2cbb9b98187ec8a4764457cb
정현서 <ox672@naver.com>
2023-12-11 23:25:20 +0900

    MVC 실습

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "minini");
+        return "greetings";//grettings.mustache 파일 반환
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
+}
